21eba9f24716165b52157efcfc3366d6a5e58cd3
Keenan Alves <keenan.alves@gmail.com>
2019-07-26 02:13:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,17 +20,9 @@ function sayHiToGrandma(string){
     return "YES INDEED!"
   } else if(string = string.toLowerCase()) {
     return "I can't hear you!" 
-  } else if(string = )
+  } else if(string = "I love you, Grandma.") {
+    return "I love you, too."
+  } else {
+    return "I can't hear you!"
   }
 } 
-
-
-= sayHiToGrandma.shout => {
-  return "YES INDEED!"
-} else if(string !== sayHiToGrandma.shout) {
-  return "I can't hear you!" 
-} else if(string = "I love you, Grandma.") {
-  return "I love you, too."
-} else {
-  console.log(sayHiToGrandma.shout);
-}
