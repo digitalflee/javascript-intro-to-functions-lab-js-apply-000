f05086f08e991fcbc3dce2a032c69a2d7e3a8afd
Keenan Alves <keenan.alves@gmail.com>
2019-07-26 02:24:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if(string !== string.toUpperCase()) {
-    return "I can't hear you!"
+    return "I can\'t hear you!"
   } else if(string === "I love you, Grandma.") {
     return "I love you, too." 
   } else {
